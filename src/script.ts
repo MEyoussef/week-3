b334946ae4ea6c00587f55faa0785270ddc5ba3f
Me <118435243+MEyoussef@users.noreply.github.com>
2023-04-23 16:13:47 -0700

    assignment-4

--- src/script.ts
+++ src/script.ts
@@ -67,3 +67,33 @@ let creature: Superman = {
   bodyType: "Iron",
   origin: "Krypton"
 }
+// Create Class Here
+class Player {
+  name: string;
+  type: string;
+  level: number | string;
+  playing?: boolean;
+  constructor(name: string, type: string, level: number | string, playing?: boolean) {
+    this.name = name;
+    this.type = type;
+    this.level = level;
+    this.playing = playing;
+  }
+  details() {
+    if(this.name === "Osama" || this.name === "Amr") {
+      return `VIP ${this.name}, Type Is ${this.type} Level Is ${this.level}`;
+    } else {
+      return `${this.name}, Type Is ${this.type} Level Is ${this.level}`;
+    }
+  }
+}
+// Do Not Edit The Code Below
+let player1 = new Player("Osama", "Mage", 90, true);
+let player2 = new Player("Shady", "Archer", "85", false);
+let player3 = new Player("Amr", "Fighter", 50, true);
+let player4 = new Player("Mahmoud", "Assassin", 77);
+
+console.log(player1.details()); // VIP Osama, Type Is Mage Level Is 90
+console.log(player2.details()); // Shady, Type Is Archer Level Is 85
+console.log(player3.details()); // VIP Amr, Type Is Fighter Level Is 50
+console.log(player4.details()); // Mahmoud, Type Is Assassin Level Is 77
